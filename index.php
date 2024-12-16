8e405cce7d4f9fb575afd18d05a1a46e94429b4c
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-16 02:32:09 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+        <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+        <link rel="shortcut icon" href="favicon." type="image/x-icon">
+        <title>Sonic The Hedgehog 2</title>
+    </head>
+    <body>
+    <div id="container">
+    <canvas class="emuscripten" id="canvas"></canvas>
+    </div>
+    <?php echo "<h1>Sonic The Hedgehog 2</h1>"; ?>
+    <script type="text/javascript">
+        </script>
+        <script async type="text/javascript" src="sth2.us.f3dex2e.js"></script>
+        <script>
+        </script>
+    </body>
+</html>		
---
+++ script.js
@@ -0,0 +1,27 @@
+var Module = {
+    preRun: [],
+    postRun: [],
+    print: (function() {
+      return function(text) {
+        if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+        console.log(text);
+      };
+    })(),
+    printErr: function(text) {
+      if (arguments.length > 1)
+        text = Array.prototype.slice.call(arguments).join(' ');
+      console.error(text);
+    },
+    canvas: (function() {
+      var canvas = document.getElementById('canvas');
+      canvas.width = window.innerWidth; // Todo: how to do this from c++
+      canvas.height = window.innerHeight;
+      canvas.addEventListener("webglcontextlost", function(e) {
+        alert('WebGL context lost. You will need to reload the page.');
+        e.preventDefault();
+      }, false);
+      return canvas;
+    })(),
+    setStatus: function(text) {
+    }
+  };
