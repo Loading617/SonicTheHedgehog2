00faf7c61ddef3d9609e6e604e76d2800c54794c
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-16 12:00:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <li>Movement and Cycle Soundtracks: Left and Right Arrow Keys</li>
             <li>Roll: Left Arrow or Right Arrows, Down Arrows</li>
             <li>Speed Up: Hold down Left or Right Arrow Keys</li>
-            <li>Look Up and Down: Up and Down Arrow Keys</li>
+            <li>Look Up and Down and Navigate Menu: Up and Down Arrow Keys</li>
             <li>Start: Enter</li>
             <li>Jump: Spacebar</li>
         </ul>
