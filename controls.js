8127cf205edde61790b0a097dca2be712d797638
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-17 22:57:33 -0500

    Delete controls.js

--- controls.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowLeft') {
-        // Move player left
-    } else if (event.key === 'ArrowRight') {
-        // Move player right
-    } else if (event.key === ' ') {
-        // Perform an attack
-    }
-});
