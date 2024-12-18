1c673b4a3c6d4115499b6d2b0894984f083f021d
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-17 19:55:40 -0500

    Update and rename script.js to controls.js

---
+++ controls.js
@@ -0,0 +1,9 @@
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        // Move player left
+    } else if (event.key === 'ArrowRight') {
+        // Move player right
+    } else if (event.key === ' ') {
+        // Perform an attack
+    }
+});
